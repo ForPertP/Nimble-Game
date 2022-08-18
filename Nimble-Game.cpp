b54b0ce10a35c2a64e129e7b603c42d72b660a16
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2022-08-19 07:24:52 +0900

    Update Nimble-Game.cpp

--- Nimble-Game.cpp
+++ Nimble-Game.cpp
@@ -1,1 +1,20 @@
+#include <bits/stdc++.h>
 
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'nimbleGame' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts INTEGER_ARRAY s as parameter.
+ */
+
+string nimbleGame(vector<int> s)
+{
+  return "First"; 
+}
+    
